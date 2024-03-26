36f322e61d2cee81e07b6a3e6e2642f6c91c6560
jeanpaullopes <jean.lopes@ulife.com.br>
2024-03-26 20:52:44 -0300

    primeiro commit

---
+++ aula4.js
@@ -0,0 +1,10 @@
+function render() {
+    const app = document.getElementById('app');
+    app.innerHTML = `
+        <h1>Olá, mundo!</h1>
+        <p>Esse é um parágrafo</p>
+        <button onclick="alert('Olá')">Clique aqui</button>
+    `;
+}
+
+
