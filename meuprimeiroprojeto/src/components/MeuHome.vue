6fd1a82c5c386a2c1947d4a15a8a7d5d8ff97cb1
jeanpaullopes <jean.lopes@ulife.com.br>
2024-04-23 20:55:35 -0300

    vue primeira aula

---
+++ meuprimeiroprojeto/src/components/MeuHome.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h3>meu Home</h3>
+        bem vind{{ letraPorSexo }} {{ nome }}<br/>
+        {{ sexo == 'F' ? 'Seja bem vinda' : 'Seja bem vindo' }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MeuHome',
+    props: {
+        nome: {type: String, required: true},
+        sexo: {type: String, default: 'M'}
+    },
+    computed: {
+        letraPorSexo() {
+            //return this.sexo === 'F' ? 'a' : 'o'
+            if (this.sexo === 'F') 
+                return 'a' 
+            else 
+                return 'o'
+        }
+    },
+}
+</script>
+
+<style>
+</style>
---
+++ meuprimeiroprojeto/src/components/PostTile.vue
@@ -0,0 +1,23 @@
+<template>
+ <div class="divPost">
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.body }}</p>
+    por: {{ post.userId }}
+ </div>
+</template>
+
+<script>
+    export default {
+        name: 'PostTile',
+        props: {
+            post: {type: Object, required: true}
+        }
+    }
+</script>
+<style>
+ .divPost {
+        border: 1px solid #ccc;
+        padding: 1rem;
+        margin: 1rem 0;
+ }
+</style>
